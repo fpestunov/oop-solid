d2c920cbeaf66062def4607dde105ae9c94633b3
fpestunov <fpestunov@gmail.com>
2020-06-13 19:14:45 +0400

    add first SRP example

---
+++ 01-SRP/Animal/input.php
@@ -0,0 +1,19 @@
+<? 
+
+class Animal {
+
+  public function constructor($name)
+  {
+    $this->name = $name;
+  }
+
+  public function getName()
+  {
+    
+  }
+
+  public function saveAnimal()
+  {
+    // save to file
+  }
+}
---
+++ 01-SRP/Animal/output.php
@@ -0,0 +1,28 @@
+<? 
+
+class Animal {
+
+  public function constructor($name)
+  {
+    $this->name = $name;
+  }
+
+  public function getName()
+  {
+    
+  }
+
+}
+
+class AnimalFileRepository {
+  public function loadAnimal()
+  {
+    // get from file
+  }
+
+  public function saveAnimal()
+  {
+    // save to file
+  }
+
+}
